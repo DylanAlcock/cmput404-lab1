6d7ba90810261ac27d974c5fcea6609e4c6c70ea
Dylan Alcock <alcock@cs.ualberta.ca>
2020-01-07 08:52:47 -0700

    Modified script

--- lab1.py
+++ lab1.py
@@ -3,3 +3,6 @@ print(requests.__version__)
 
 google = requests.get("https://google.com")
 print(google)
+
+var = requests.get("https://raw.githubusercontent.com/DylanAlcock/cmput404-lab1/master/lab1.py")
+print(var.content)
